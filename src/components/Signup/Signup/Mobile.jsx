3426b00293b4181f796c32f69b95c3a93550693e
youngju6143 <youngju6143@naver.com>
2024-07-26 19:13:29 +0900

    refactor: 회원가입 모바일/PC 통합

--- src/components/Signup/Signup/Mobile.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import SignupCm from './SignupCm'
-
-export default function Mobile() {
-  return (
-    <SignupCm/>
-  )
-}
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Mobile from '../components/Signup/Signup/Mobile';
-import Browser from '../components/Signup/Signup/Browser';
 import SignupCm from '../components/Signup/Signup/SignupCm';
 
 export default function Signup() {

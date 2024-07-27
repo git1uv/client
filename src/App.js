8014deca4bcacd83517871386250172b4cee916a
youngju6143 <youngju6143@naver.com>
2024-07-27 17:06:17 +0900

    feat: 로그인/회원가입 헤더 비활성화

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import MyCalendar from './components/MyCalendar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import DatePage from './routes/DatePage';
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
 import SettingName from './routes/SettingName';
 
 function App() {
+  const location = useLocation();
+  const noHeaderRoutes = ['/login', '/signup', '/signup/nickname', '/'];
+
+  const isShowHeader = !noHeaderRoutes.includes(location.pathname);
+
+  console.log(window.innerWidth)
+  console.log(window.innerHeight)
   return (
     <div>
-        <Header />
+        {isShowHeader && <Header />}
         <Routes>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>

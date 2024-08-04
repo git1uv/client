bb800cb816ecc717e9a061a8cf2b9bbffc807a06
youngju6143 <youngju6143@naver.com>
2024-08-05 04:31:10 +0900

    design: 연필꽂이 레이아웃 구현 중

--- src/components/PencilHolder/PencilHolder.style.js
+++ src/components/PencilHolder/PencilHolder.style.js
@@ -5,7 +5,7 @@ import backgroundImg from "../../assets/pencilHolder/bgImg.png"
 export const Container = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-end;
     flex-direction: column;
 
     width: 100%;
@@ -23,6 +23,7 @@ export const Container = styled.div`
     width: 100%;
     height: 100%;
     backdrop-filter: blur(3px);
+    background: rgba(255, 255, 255, 0.30);
   }
   & > * {
     position: relative;
@@ -32,13 +33,20 @@ export const Container = styled.div`
     @media (max-width: 430px) {
         height: 100dvh;
     }
+    
+    section {
+        height: 8rem;
+        @media (max-width: 1440px) {
+            height: 5rem;
+        }
+    }
 `;
 
 export const Title = styled.div`
     display: flex;
     align-items: flex-start;
     flex-direction: column;
-    top: 4rem;
+    height: 10vh;
 
     h1 {
         font-family: 'Pretendard';
@@ -46,6 +54,10 @@ export const Title = styled.div`
         font-size: 2.5rem;
         letter-spacing: -0.408px;
         color: #000000;
+
+        @media (max-width: 1440px) {
+            font-size: 2rem;
+        }
     }
     p {
         margin: 0;
@@ -62,16 +74,88 @@ export const Letter = styled.div`
     flex-direction: column;
     align-items: center;
 
-    top: 8rem;
-    width: 48.25rem;
-    height: 70%;
+    width: 50rem;
+    height: 80vh;
 
     @media (max-width: 1440px) {
-        top: 2.5rem;
-        width: 50%;
+        width: 50rem;
+        height: 65vh;
+    }
+    @media (max-width: 1200px) {
+        width: 50rem;
+        height: 56vh;
     }
     background-image: url(${letter});
     background-size: 100%;
-    background-position: contain;
+    background-position: cover;
     background-repeat: no-repeat;
+`
+
+export const SizedBox = styled.div`
+    height: 5rem;
+
+    @media (max-width: 1440px) {
+        height: 4rem;
+    }
+    @media (max-width: 1200px) {
+        height: 4rem;
+    }
+`
+
+export const WriteBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 40rem;
+    margin-top: 1.5rem;
+
+    h3 {
+        color: #ED6243;
+        font-family: Pretendard;
+        font-size: 1rem;
+        font-weight: 500;
+        line-height: 1.375rem; /* 146.667% */
+        letter-spacing: -0.0255rem;
+    }
+
+    input {
+        width: 22rem;
+        height: 3rem;
+        border: none;
+        border-radius: 0.3125rem;
+        background: #FFF;
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+
+        font-family: SeoulHangang;
+        font-size: 1.25rem;
+        font-weight: 400;
+        line-height: normal;
+        padding-left: 10px;
+    }
+
+    input::placeholder {
+        color: #A3A3A3;
+    }
+    
+    input:focus {
+        outline: none;
+    }
+
+    textarea {
+        width: 41rem;
+        height: 40vh;
+        @media (max-width: 1440px) {
+            height: 25vh;
+        }
+
+        border: none;
+        border-radius: 1rem;
+        background: #FFF;
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+
+        resize: none;
+    }
+    textarea:focus {
+        outline: none;
+    }
 `

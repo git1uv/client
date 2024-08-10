7a4243786e6f6ad11a52f51f7740fde97cc775d2
hyeran1216 <hran1216@khu.ac.kr>
2024-08-10 23:55:15 +0900

    feat: 애니메이션 추가

--- src/assets/trashImg/icons.js
+++ src/assets/trashImg/icons.js
@@ -1,5 +1,6 @@
 
 import trashImage from './image_trash_back_web.png';
+import trashImageFront from './image_trash_front_web.png';
 import trashImageMobile from './image_trash_1.png';
 import card1ImageMobile from './card_up-1.png';
 import card1ImageWeb from './card_up.png';
@@ -14,5 +15,5 @@ import throwOutImageMobile from './button_delete_mobile.png';
 import backgroundImageMobile from './image_bg_mobile.png';
 import backgroundImageWeb from './image_bg.png';
 
-export {trashImage,trashImageMobile, card1ImageMobile, card1ImageWeb, card2ImageMobile, card2ImageWeb, buttonImageWeb, buttonImageMobile, paperImageMobile, paperImageWeb, throwOutImageWeb, throwOutImageMobile, backgroundImageMobile, backgroundImageWeb};
+export {trashImage,trashImageFront, trashImageMobile, card1ImageMobile, card1ImageWeb, card2ImageMobile, card2ImageWeb, buttonImageWeb, buttonImageMobile, paperImageMobile, paperImageWeb, throwOutImageWeb, throwOutImageMobile, backgroundImageMobile, backgroundImageWeb};
 
--- src/components/Trash/DeletePaperModal.jsx
+++ src/components/Trash/DeletePaperModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as M from '../Modal/LogoutModal.style';
 import x from '../../assets/x.png';
 import { useNavigate } from 'react-router-dom';
@@ -7,6 +7,14 @@ import gif from '../../assets/trashImg/delete.gif'
 const DeletePaperModal = ({ isVisible, onClose, onConfirm }) => {
   const outside = useRef();
   const navigate = useNavigate();
+  const [gifSrc, setGifSrc] = useState(gif);
+
+  useEffect(() => {
+    if (isVisible) {
+      setGifSrc(`${gif}?${new Date().getTime()}`);
+    }
+  }, [isVisible]);
+
 
   if (!isVisible) return null;
 
@@ -14,7 +22,7 @@ const DeletePaperModal = ({ isVisible, onClose, onConfirm }) => {
     <M.LogoutModalBg ref={outside} onClick={(e) => { if (e.target === outside.current) onClose(); }}>
       <M.LogoutModal>
         <M.ModalCloseButton onClick={onClose}><img src={x} alt='x' /></M.ModalCloseButton>
-        <M.GifImage src={gif} alt="GIF" /> 
+        <M.DeleteGif src={gifSrc} alt="GIF" /> 
         <M.LogoutModalTitle>작성한 내용을 쓰레기통에 버렸어요!</M.LogoutModalTitle>
         <M.ModalActions>
           <M.CancelButton onClick={() => navigate('/main')}>홈으로 돌아가기</M.CancelButton>

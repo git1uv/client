fdcc52177853656eeeaca81be64de7761a0ff5a0
youngju6143 <youngju6143@naver.com>
2024-08-16 17:50:33 +0900

    design: input 추가, 챗봇 정보 레이아웃

--- src/routes/Chatbot/Chatbot.jsx
+++ src/routes/Chatbot/Chatbot.jsx
@@ -4,6 +4,9 @@ import Chair from '../../assets/chatbot/ChairWeb.png'
 import Simmaeum from '../../assets/chatbot/Simmaeum.png'
 import Banbani from '../../assets/chatbot/Banbani.png'
 import Neuranee from '../../assets/chatbot/Neuranee.png'
+import howto from '../../assets/chatbot/howto.png'
+import func from '../../assets/chatbot/function.png'
+import warning from '../../assets/chatbot/warning.png'
 
 export default function Chatbot() {
   return (
@@ -20,17 +23,42 @@ export default function Chatbot() {
         </S.Header>
         <S.Container>
           <S.Title>
-            <p>챗봇과 대화하기</p>
+            <h1>챗봇과 대화하기</h1>
           </S.Title>
           <S.InfoBox>
-            <S.Infos></S.Infos>
-            <S.Infos></S.Infos>
-            <S.Infos></S.Infos>
+            <S.Infos>
+              <S.IconBox id='howto'>
+                <img src={howto} alt='icon' />
+                <h2>사용법</h2>
+              </S.IconBox>
+              <S.Info></S.Info>
+            </S.Infos>
+            <S.Infos>
+              <S.IconBox id='func'>
+                <img src={func} alt='icon' />
+                <h2>기능</h2>
+              </S.IconBox>
+              <S.Info></S.Info>
+              <S.Info></S.Info>
+              <S.Info></S.Info>
+            </S.Infos>
+            <S.Infos>
+              <S.IconBox id='warning'>
+                <img src={warning} alt='icon' />  
+                <h2>주의</h2>
+              </S.IconBox>
+              <S.Info></S.Info>
+            </S.Infos>
           </S.InfoBox>
           <S.Warning>
             <h3>* 놀라지 마세요!</h3>
             <p>대화 도중 더 전문적인 상담이 필요한 경우 알려드립니다.</p>
           </S.Warning>
+          <S.InputBox>
+            <input
+              placeholder='고민부터 털어놓고 싶은 것, 오늘 있었던 일 등 뭐든 말해보아요!'></input>
+            <button />
+          </S.InputBox>
         </S.Container>
       </S.Bottom>
     </S.App>
--- src/routes/Chatbot/Chatbot.style.js
+++ src/routes/Chatbot/Chatbot.style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import backgroundImg from '../../assets/backgroundImg.png'
+import sendBtn from '../../assets/chatbot/sendBtn.png'
 import { IoIosArrowBack } from "react-icons/io";
 import theme from '../../constants/theme'; // theme 파일의 경로
 
@@ -71,15 +72,19 @@ export const Bottom = styled.div`
     width: 70%;
     min-width: 500px;
     height: 65vh;
-    background-color: #F5F4F2;
+
     border-radius: 2.5rem;
+    border: 1px solid #FFF;
+    background: rgba(243, 243, 243, 0.85);
+    box-shadow: 3px 2px 20px 0px rgba(95, 95, 95, 0.25) inset;
+    backdrop-filter: blur(10px);
 `
 
 /* ChatbotBox*/
 
 export const Header = styled.div`
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: center;
     padding: 0 5%;
 
@@ -89,25 +94,40 @@ export const Header = styled.div`
     border-radius: 2.5rem 2.5rem 0 0;
     filter: drop-shadow(0px 4px 4px rgba(165, 165, 165, 0.25));
 
-    button {
-    width: 6.8rem;
-    height: 2.8rem;
-    flex-shrink: 0;
-    border-radius: 4.8125rem;
-    background: #ED6243;
-    border: none;
+    h1 {
+        width: 80%;
+        text-align: center;
 
-    color: white;
-    font-family: Pretendard;
-    font-size: 1.25rem;
-    font-weight: 700;
-    line-height: normal;
-}
+        color: #27272A;
+        font-family: SeoulHangang;
+        font-size: 1.5rem;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: -0.015rem;
+    }
+    button {
+        width: 6.8rem;
+        // width: 15%;
+        height: 2.8rem;
+        margin-right: -4%;
+
+        border-radius: 4.8125rem;
+        background: #ED6243;
+        border: none;
+
+        color: white;
+        font-family: Pretendard;
+        font-size: 1.25rem;
+        font-weight: 700;
+        line-height: normal;
+    }
 `
 
 export const BackBtn = styled(IoIosArrowBack)`
-    width: 2rem;
+    // width: 2rem;
+    width: 15%;
     height: 2rem;
+    margin-left: -8%;
     color: #ED6243;
 `
 
@@ -117,22 +137,68 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    position: relative;
 
     width: 90%;
     height: 100%;
 `
 
 export const Title = styled.div`
-    ${ALIGN.ROW_CENTER} ;
-
+    ${ALIGN.ROW_CENTER};
     width: 100%;
     height: 15%;
-    background-color: pink;
+
+    h1 {
+        margin: 0;
+        color: #27272A;
+        font-family: SeoulHangang;
+        font-size: 1.5rem;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: -0.015rem;
+    }
 `
 export const Warning = styled.div`
     h3 {}
     p {}
 `
+
+export const InputBox = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    
+    width: 80%;
+    height: 4rem;
+    
+    position: absolute;
+    bottom: calc(0% - 2rem);
+    left: calc(50% - 40%);
+
+    border-radius: 2.0625rem;
+    border: 1px solid #739B8B;
+    background: #FFF;
+    box-shadow: 3px 1px 10px 0px #8F9988;
+    
+    input {
+        width: 80%;
+        height: 3rem;
+
+        background: none;
+        border: none;
+        outline: none;
+    }
+
+    button {
+        width: 3.125rem;
+        height: 3.125rem;
+        border: none;
+        background-color: transparent;
+        background-image: url(${sendBtn});
+        background-size: cover;
+    }
+`
+
 /* ChatbotInfo*/
 
 export const InfoBox = styled.div`
@@ -157,5 +223,26 @@ export const Infos = styled.div`
 
 export const IconBox = styled.div`
     ${ALIGN.COLUMN_CENTER};
+    padding: 3% 0;
+    img {
+        width: ${({ id }) => id === 'howto' ? '1.8rem' : id === 'func' ? '2.2rem' : '2.8rem'};
+        height: 2.4rem;
+        object-fit: cover;
+    }
 
+    h2 {
+        margin: 1% 0 0 0;
+        color: #27272A;
+        font-family: Pretendard;
+        font-size: 1.125rem;
+        font-weight: 600;
+        line-height: normal;
+        letter-spacing: -0.01125rem;
+    }
+`
+
+export const Info = styled.div`
+    width: 100%;
+    border: 1px black solid;
+    height: 100%;
 `

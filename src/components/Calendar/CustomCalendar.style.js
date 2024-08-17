ea8033d8cded72a1748d30fbf79fb2ce4ad0d687
hyeran1216 <hran1216@khu.ac.kr>
2024-08-17 17:57:01 +0900

    style: 달력 내비게이션 바

--- src/components/Calendar/CustomCalendar.style.js
+++ src/components/Calendar/CustomCalendar.style.js
@@ -10,10 +10,13 @@ export const StyledCalendarWrapper = styled.div`
   height: 84.5vh;
   position: relative;
   @media screen and (min-width: 431px) {
-    margin-top: 10.3vh;
+    margin-top: 5.3vh;
   }
   @media screen and (max-width: 430px) {
-    height: 70vh; 
+    height: 75vh; 
+    width: 100%;
+  }
+
   }
   .react-calendar {
     width: 100%;
@@ -35,9 +38,12 @@ export const StyledCalendarWrapper = styled.div`
   
   /* 네비게이션 폰트 설정 */
   .react-calendar__navigation button {
-    font-weight: 800;
+    font-weight: bold;
     font-size: 2rem;
-    color: black;
+    font-family: SeoulHangang;
+    line-height: 22px; 
+    color: #4C434E;
+    margin: 0 1vw;
   }
   
   /* 네비게이션 버튼 컬러 */
@@ -53,24 +59,28 @@ export const StyledCalendarWrapper = styled.div`
   /* 요일 밑줄 제거 */
   .react-calendar__month-view__weekdays abbr {
     text-decoration: none;
-    font-weight: 800;
-    font-size: 1.5rem;
+    font-weight: 400;
+    font-size: 1.125rem;
+    line-height: 22px; 
+    color: #4C434E;
+    font-family: Pretendard;
   }
   .react-calendar__month-view__weekdays{
-    margin-bottom: 3vh;
+    margin-bottom: 1vh;
   }
-  
-  /* 일요일에만 빨간 폰트 */
-  .react-calendar__month-view__weekdays__weekday--weekend abbr[title="일요일"] {
-    color: red;
+  .react-calendar__month-view__days {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
   }
-  .react-calendar__month-view__days__day--weekend:nth-child(7n+6) abbr {
-    color: black;
-  }
-  /* 월, 화, 수, 목, 금요일의 글자 색을 검정색으로 설정 */
-  .react-calendar__month-view__days__day:not(.react-calendar__month-view__days__day--weekend) abbr,
-  .react-calendar__month-view__weekdays__weekday:not(.react-calendar__month-view__weekdays__weekday--weekend) abbr {
-      color: black;
+  .react-calendar__month-view__days__day abbr { 
+    color: #78716C;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px; 
   }
 
   
@@ -81,13 +91,6 @@ export const StyledCalendarWrapper = styled.div`
   }
   .react-calendar__tile--now abbr {
     color: orange;
-  }
-  
-  /* 네비게이션 월 스타일 적용 */
-  .react-calendar__year-view__months__month {
-    border-radius: 0.8rem;
-    background-color:gray;
-    padding: 0;
   }
   
   /* 네비게이션 현재 월 스타일 적용 */
@@ -107,23 +110,7 @@ export const StyledCalendarWrapper = styled.div`
   
   /* 네비게이션 월 스타일 적용 */
   .react-calendar__year-view__months__month {
-    flex: 0 0 calc(33.3333% - 10px) !important;
-    margin-inline-start: 5px !important;
-    margin-inline-end: 5px !important;
-    margin-block-end: 10px;
-    padding: 20px 6.6667px;
-    font-size: 0.9rem;
-    font-weight: 600;
-    color: black;
-  }
   
-  /* 선택한 날짜 스타일 적용 */
-  .react-calendar__tile:enabled:hover,
-  .react-calendar__tile:enabled:focus,
-  .react-calendar__tile--active {
-    background-color: #e0e0e0;
-    border-radius: 0.8rem;
-    color: black;
   }
   
 `;
@@ -140,15 +127,19 @@ export const StyledToday = styled.div`
 `;
 
 export const StyledEmotion = styled.div`
-  background-image: url('../../assets/smile.png');
+  background-image: ${({ imageUrl }) => imageUrl ? `url(${imageUrl})` : 'none'};
   background-size: cover;
   background-position: center;
   border-radius: 50%;
-  width: 1rem;
-  height: 1rem;
+  width: 6.25rem;
+  height: 6.25rem;
   position: absolute;
   top: 60%;
   left: 50%;
   transform: translateX(-50%);
+  ${({ imageUrl }) => !imageUrl && `
+    border: 1px solid ##F4F2EB; 
+  `}
+
 `;
 
--- src/routes/MyCalendar.jsx
+++ src/routes/MyCalendar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from "moment";
 import styled from "styled-components";
 import * as C from '../components/Calendar/CustomCalendar.style';
 import { useNavigate } from 'react-router-dom';
+import leftArrow from '../assets/CalendarImg/left-arrow.png'; 
+import rightArrow from '../assets/CalendarImg/right-arrow.png'; 
 const CalendarWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -16,13 +18,49 @@ const CalendarWrapper = styled.div`
 
 function MyCalendar() {
   const [value, onChange] = useState(new Date());
+  const [emotionData, setEmotionData] = useState([]);
   const navigate = useNavigate();
   const today = new Date();
   const emotion_day = [
-    '2024-07-20',
-    '2024-07-21',
+    '2024-08-20',
+    '2024-08-21',
     // 여기에 추가
   ];
+
+  /**useEffect(() => {
+    fetch('/api/v1/calendar/:month/home')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 200) {
+          setEmotionData(data.data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching calendar data:', error);
+      });
+  }, []);
+
+  const renderEmotionIcon = (date) => {
+    const formattedDate = moment(date).format('YYYY-MM-DD');
+    const emotionEntry = emotionData.find(entry => entry.date === formattedDate);
+    
+    if (emotionEntry) {
+      switch (emotionEntry.emotion) {
+        case 'happy':
+          return <C.StyledEmotion imageUrl={happyIcon} />;
+        case 'sad':
+          return <C.StyledEmotion imageUrl={sadIcon} />;
+        // 감정 케이스를 추가
+        default:
+          return <C.StyledEmotion />;
+      }
+    } else {
+      return <C.StyledEmotion />;
+    }
+  };
+  */
+
+
   const handleDateClick = (date) => {
     const formattedDate = moment(date).format('YYYY-MM-DD');
     navigate(`/date/${formattedDate}`);
@@ -35,6 +73,10 @@ function MyCalendar() {
       <Calendar
         onChange={onChange}
         value={value}
+        minDetail="year"
+        maxDetail="month"
+        prevLabel={<img src={leftArrow} alt="Previous" />}
+        nextLabel={<img src={rightArrow} alt="Next" />}
         next2Label={null}
         prev2Label={null}
         formatDay={(locale, date) => moment(date).format('D')}
@@ -54,6 +96,10 @@ function MyCalendar() {
           if (emotion_day.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
             html.push(<C.StyledEmotion key={moment(date).format("YYYY-MM-DD")} />);
           }
+          //if (view === 'month') {
+          //  return renderEmotionIcon(date);}
+          //return null;}}
+
           return <>{html}</>;
         }}
       />

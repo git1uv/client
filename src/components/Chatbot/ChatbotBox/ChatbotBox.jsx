d11c51cb14d69fe83dce01d1d6ab8a6cee9c450b
youngju6143 <youngju6143@naver.com>
2024-08-21 03:37:48 +0900

    refactor: 챗봇 폴더 리팩토링

--- src/components/Chatbot/ChatbotBox/ChatbotBox.jsx
+++ src/components/Chatbot/ChatbotBox/ChatbotBox.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import * as S from './ChatbotBox.style'
+import ChatbotInfo from '../ChatbotInfo/ChatbotInfo'
 
 export default function ChatbotBox() {
   return (
-    <div>ChatbotBox</div>
+    <S.Container>
+      <S.Title>
+        <h1>챗봇과 대화하기</h1>
+      </S.Title>
+      <ChatbotInfo />
+      <S.Warning>
+        <h3>* 놀라지 마세요!</h3>
+        <p>대화 도중 더 전문적인 상담이 필요한 경우 알려드립니다.</p>
+      </S.Warning>
+      <S.InputBox>
+        <input
+          placeholder='고민부터 털어놓고 싶은 것, 오늘 있었던 일 등 뭐든 말해보아요!'></input>
+        <button />
+      </S.InputBox>
+    </S.Container>
   )
 }
--- src/routes/Chatbot/Chatbot.jsx
+++ src/routes/Chatbot/Chatbot.jsx
@@ -4,9 +4,8 @@ import Chair from '../../assets/chatbot/ChairWeb.png'
 import Simmaeum from '../../assets/chatbot/Simmaeum.png'
 import Banbani from '../../assets/chatbot/Banbani.png'
 import Neuranee from '../../assets/chatbot/Neuranee.png'
-import howto from '../../assets/chatbot/howto.png'
-import func from '../../assets/chatbot/function.png'
-import warning from '../../assets/chatbot/warning.png'
+
+import ChatbotBox from '../../components/Chatbot/ChatbotBox/ChatbotBox';
 
 export default function Chatbot() {
   return (
@@ -21,54 +20,7 @@ export default function Chatbot() {
           <h1>심마음</h1>
           <button>끝내기</button>
         </S.Header>
-        <S.Container>
-          <S.Title>
-            <h1>챗봇과 대화하기</h1>
-          </S.Title>
-          <S.InfoBox>
-            <S.Infos id='howto'>
-              <S.IconBox id='howto'>
-                <img src={howto} alt='icon' />
-                <h2>사용법</h2>
-              </S.IconBox>
-              <S.Info>
-                <p>“나 오늘 이렇고 이런 일이 있었는데...”</p>
-                <p>챗봇과 채팅을 통해 자신의 마음을 솔직하게 털어놓고 캐릭터의 일지를 통해 자신의 감정을 정리해보아요</p>
-              </S.Info>
-            </S.Infos>
-            <S.Infos>
-              <S.IconBox id='func'>
-                <img src={func} alt='icon' />
-                <h2>기능</h2>
-              </S.IconBox>
-              <S.Info id='func'>
-                <p>현재 나의 감정을 마음껏 털어놓을 수 있어요 </p>
-              </S.Info>
-              <S.Info id='func'>
-                <p>나의 상황에 대해 이 캐릭터가 공감/조언해줘요</p>
-              </S.Info>
-              <S.Info id='func'>
-                <p>대화내용을 정리해주고 일지를 작성해줘요</p>
-              </S.Info>
-            </S.Infos>
-            <S.Infos id='warning'>
-              <S.IconBox id='warning'>
-                <img src={warning} alt='icon' />  
-                <h2>주의</h2>
-              </S.IconBox>
-              <S.Info></S.Info>
-            </S.Infos>
-          </S.InfoBox>
-          <S.Warning>
-            <h3>* 놀라지 마세요!</h3>
-            <p>대화 도중 더 전문적인 상담이 필요한 경우 알려드립니다.</p>
-          </S.Warning>
-          <S.InputBox>
-            <input
-              placeholder='고민부터 털어놓고 싶은 것, 오늘 있었던 일 등 뭐든 말해보아요!'></input>
-            <button />
-          </S.InputBox>
-        </S.Container>
+        <ChatbotBox />
       </S.Bottom>
     </S.App>
   );

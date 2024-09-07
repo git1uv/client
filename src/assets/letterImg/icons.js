e7134d6b4eaa99a5999ac4b21965e9fcbab5de48
hyeran1216 <hran1216@khu.ac.kr>
2024-09-08 04:01:32 +0900

    docs: 이미지 추가

---
+++ src/assets/letterImg/icons.js
@@ -0,0 +1,10 @@
+import notreadO from './button_didntread_o.png';
+import notreadX from './buttton_didntread_x.png';
+import allO from './button_everything_o.png';
+import allX from './button_everything_x.png';
+import favoriteO from './buttton_favourites_o.png';
+import favoriteX from './buttton_favourites_x.png';
+import mailbox from './mailbox.png';
+import mailbox_mobile from './Mask group.png';
+
+export {notreadO, notreadX, allO, allX, favoriteO, favoriteX, mailbox, mailbox_mobile};
---
+++ src/routes/Chatbot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chatbot() {
+  return (
+    <div>
+      <h1>챗봇 페이지</h1>
+    </div>
+  );
+}
+
+export default Chatbot;

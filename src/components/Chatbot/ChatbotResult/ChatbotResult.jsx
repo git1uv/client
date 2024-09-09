3e8392c9e692e010e37faed17acfb1e3516ae031
youngju6143 <youngju6143@naver.com>
2024-09-09 13:42:47 +0900

    feat: 챗봇 결과 틀만 잡음

--- src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
+++ src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
@@ -1,7 +1,85 @@
 import React from 'react'
-
+import * as S from './ChatbotResult.style'
 export default function ChatbotResult() {
   return (
-    <div>ChatbotResult</div>
+    <S.App>
+      <S.Container>
+        <S.Top>
+          <S.Header>
+            <img src="/path-to-your-image.png" alt="Character" />
+          </S.Header>
+          <S.Name>
+            <div>심마음의 일지</div>
+            <h3>2024 / 00 / 00</h3>
+          </S.Name>
+        </S.Top>
+      <S.Bottom>
+        <S.ContentBox>
+          <S.Title>
+            <img />
+            <h2>네가 가진 고민은 이런 거야</h2>
+          </S.Title>
+          <div>
+            <p>
+              더하지 않아도 대충 이젠 감이 온다. 
+              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
+              창업을 여럿해본 미국 유학파 CEO와 빛나는 이력의 CTO. 
+              눈이 반짝반짝 빛나는, 함께 있기 편한, 사람 좋은 S급 개발자들. 
+              여기에 더해 다양한 나라에서 온 사업개발 전문가들. 
+              (총 10개국이 넘는 분들이 모인 팀이었다). 
+              이런 완벽한 팀에, 백억원이 넘는 시드 투자까지. 
+              가장 똑똑한 사람들과 듣기만해도 부풀어 오르는 신나는 프로젝트들이 넘쳐났다. 
+              기존 B2C, 농업 커머스에서 내가 처음으로 IT 분야에 발딛게 된 순간이 었고, 
+              하루 하루의 출근이 기
+            </p>
+          </div>
+        </S.ContentBox>
+        <S.ContentBox>
+          <S.Title>
+            <img />
+            <h2>내가 생각하는 제안은 이런 것이야</h2>
+          </S.Title>
+          <div>
+            <p>
+              더하지 않아도 대충 이젠 감이 온다. 
+              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
+              창업을 여럿해본 미국 유학파 CEO와 빛나는 이력의 CTO. 
+              눈이 반짝반짝 빛나는, 함께 있기 편한, 사람 좋은 S급 개발자들. 
+              여기에 더해 다양한 나라에서 온 사업개발 전문가들. 
+              (총 10개국이 넘는 분들이 모인 팀이었다). 
+              이런 완벽한 팀에, 백억원이 넘는 시드 투자까지. 
+              가장 똑똑한 사람들과 듣기만해도 부풀어 오르는 신나는 프로젝트들이 넘쳐났다. 
+              기존 B2C, 농업 커머스에서 내가 처음으로 IT 분야에 발딛게 된 순간이 었고, 
+              하루 하루의 출근이 기
+            </p>
+          </div>
+        </S.ContentBox>
+        <S.ContentBox>
+          <S.Title>
+            <img />
+            <h2>이것을 하면 기분이 좋아질 거야</h2>
+          </S.Title>
+          <div>
+            <p>
+              더하지 않아도 대충 이젠 감이 온다. 
+              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
+            </p>
+          </div>
+          <div>
+            <p>
+              더하지 않아도 대충 이젠 감이 온다. 
+              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
+            </p>
+          </div>
+          <div>
+            <p>
+              더하지 않아도 대충 이젠 감이 온다. 
+              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
+            </p>
+          </div>
+        </S.ContentBox>
+      </S.Bottom>
+    </S.Container>
+    </S.App>
   )
 }
---
+++ src/components/Chatbot/ChatbotResult/ChatbotResult.style.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import theme from '../../../constants/theme'
+
+const {ALIGN} = theme;
+
+export const App = styled.div`
+    ${ALIGN.COLUMN_CENTER};
+    margin-top: 200px;
+`
+export const Container = styled.div`
+
+  background-color: #f2f2f2; // 배경 색상 설정
+  width: 80%; // 컴포넌트 너비 설정
+  height: auto; // 높이는 내용에 맞게 자동 설정
+  border-radius: 20px; // 직사각형 부분의 모서리를 둥글게 설정
+  position: relative;
+  padding: 20px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); // 그림자 효과
+`;
+
+export const Top = styled.div`
+    display: flex;
+`
+
+export const Header = styled.div`
+  width: 100px; // 원의 너비와 높이를 설정
+  height: 100px;
+  border-radius: 50%; // 원형을 만들기 위해 50%로 설정
+  background-color: #fff; // 원의 배경 색상
+  position: absolute; // 원을 컨테이너의 상대적 위치에 놓기 위해 사용
+  top: -50px; // 컨테이너 상단에서 50px 위로 이동
+  left: 20px; // 컨테이너 왼쪽에서 20px 떨어뜨림
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); // 그림자 효과
+
+  img {}
+`;
+
+export const Name = styled.div` // 000의 일지
+    margin-left: 10rem; 
+    div{}
+    h3 {}
+`
+
+export const Bottom = styled.div`
+  margin-top: 60px; // 원에 의한 상단 여백 추가
+  display: grid;
+grid-template-columns: repeat(3, 1fr); /* 3개의 동일한 열 */
+gap: 1rem;
+
+`;
+
+export const ContentBox = styled.div`
+
+div {}
+
+`
+
+export const Title = styled.div`
+    img {}
+    h2 {}
+`
+
+export const BtnBox = styled.div`
+    button {
+    &:first-child {}
+    $:last-child {}
+    }
+`

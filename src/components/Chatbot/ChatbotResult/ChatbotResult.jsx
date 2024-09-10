e5e2be02ef06806f198a52cf239db448e11f29df
youngju6143 <youngju6143@naver.com>
2024-09-10 10:26:23 +0900

    feat: 상담일지 내용 가져오기 (현재 더미데이터 넣어놓음)

--- src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
+++ src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import * as S from './ChatbotResult.style'
 import Moment from 'moment'
+import { useSelector } from 'react-redux';
 
 export default function ChatbotResult() {
   const [chatbot, setChatbot] = useState('');
+  const chatResult = useSelector((state) => state.chatResult);
 
   const formatDate = Moment().format('YYYY / MM / DD');
   console.log(formatDate);
@@ -37,18 +39,7 @@ export default function ChatbotResult() {
             <h2>네가 가진 고민은 이런 거야</h2>
           </S.Title>
           <div>
-            <p>
-              더하지 않아도 대충 이젠 감이 온다. 
-              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
-              창업을 여럿해본 미국 유학파 CEO와 빛나는 이력의 CTO. 
-              눈이 반짝반짝 빛나는, 함께 있기 편한, 사람 좋은 S급 개발자들. 
-              여기에 더해 다양한 나라에서 온 사업개발 전문가들. 
-              (총 10개국이 넘는 분들이 모인 팀이었다). 
-              이런 완벽한 팀에, 백억원이 넘는 시드 투자까지. 
-              가장 똑똑한 사람들과 듣기만해도 부풀어 오르는 신나는 프로젝트들이 넘쳐났다. 
-              기존 B2C, 농업 커머스에서 내가 처음으로 IT 분야에 발딛게 된 순간이 었고, 
-              하루 하루의 출근이 기
-            </p>
+            <p>{chatResult.first}</p>
           </div>
         </S.ContentBox>
         <S.ContentBox>
@@ -57,18 +48,7 @@ export default function ChatbotResult() {
             <h2>내가 생각하는 제안은 이런 것이야</h2>
           </S.Title>
           <div>
-            <p>
-              더하지 않아도 대충 이젠 감이 온다. 
-              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
-              창업을 여럿해본 미국 유학파 CEO와 빛나는 이력의 CTO. 
-              눈이 반짝반짝 빛나는, 함께 있기 편한, 사람 좋은 S급 개발자들. 
-              여기에 더해 다양한 나라에서 온 사업개발 전문가들. 
-              (총 10개국이 넘는 분들이 모인 팀이었다). 
-              이런 완벽한 팀에, 백억원이 넘는 시드 투자까지. 
-              가장 똑똑한 사람들과 듣기만해도 부풀어 오르는 신나는 프로젝트들이 넘쳐났다. 
-              기존 B2C, 농업 커머스에서 내가 처음으로 IT 분야에 발딛게 된 순간이 었고, 
-              하루 하루의 출근이 기
-            </p>
+            <p>{chatResult.second}</p>
           </div>
         </S.ContentBox>
         <S.ContentBox>
@@ -77,22 +57,13 @@ export default function ChatbotResult() {
             <h2>이것을 하면 기분이 좋아질 거야</h2>
           </S.Title>
           <div>
-            <p>
-              더하지 않아도 대충 이젠 감이 온다. 
-              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
-            </p>
+            <p>{chatResult.third[0]}</p>
           </div>
           <div>
-            <p>
-              더하지 않아도 대충 이젠 감이 온다. 
-              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
-            </p>
+            <p>{chatResult.third[1]}</p>
           </div>
           <div>
-            <p>
-              더하지 않아도 대충 이젠 감이 온다. 
-              내가 초창기 멤버로 들어간 핀테크 스타트업이 거의 비슷했다. 
-            </p>
+            <p>{chatResult.third[2]}</p>
           </div>
         </S.ContentBox>
       </S.Bottom>

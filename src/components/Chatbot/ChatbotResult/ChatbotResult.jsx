96e1283882043a7bf3b3388e9936f3e26eb84497
youngju6143 <youngju6143@naver.com>
2024-09-13 09:55:18 +0900

    feat: 다운로드 기능 구현

--- src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
+++ src/components/Chatbot/ChatbotResult/ChatbotResult.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as S from './ChatbotResult.style'
 import Moment from 'moment'
 import { useSelector } from 'react-redux';
+import html2canvas from 'html2canvas';
+import saveAs from 'file-saver';
 
 import Simmaeum from '../../../assets/chatbot/result/Simmaeum.png'
 import Banbani from '../../../assets/chatbot/result/Banbani.png'
@@ -13,14 +15,32 @@ import Icon3 from '../../../assets/chatbot/result/journal3.png'
 
 export default function ChatbotResult() {
   const [chatbot, setChatbot] = useState('');
+  const componentRef = useRef(null); 
   const chatResult = useSelector((state) => state.chatResult);
 
   const formatDate = Moment().format('YYYY / MM / DD');
   console.log(formatDate);
 
-  let result = localStorage.getItem('result');
-  useEffect(() => {
+  const handleDownload = async () => {
+    if (!componentRef.current) return;
 
+    try {
+        const canvas = await html2canvas(componentRef.current, { scale: 2 });
+        canvas.toBlob((blob) => {
+            if (blob !== null) {
+                saveAs(blob, "Counseling diary.png");
+            }
+        });
+    } catch (error) {
+        console.error("Error converting div to image:", error);
+    }
+
+};
+
+
+  let result = localStorage.getItem('result');
+  
+  useEffect(() => {
     if (result === 'Simmaeum')
       setChatbot('심마음');
     else if (result === 'Banbani')
@@ -30,7 +50,7 @@ export default function ChatbotResult() {
   }, [])
   return (
     <S.App>
-      <S.Container>
+      <S.Container ref={componentRef}>
         <S.Top>
           <S.Header>
             <img src={result === 'Simmaeum' ? Simmaeum : result === 'Banbani' ? Banbani : Neuranee} alt="Character" />
@@ -40,42 +60,46 @@ export default function ChatbotResult() {
             <h3>{formatDate}</h3>
           </S.Name>
         </S.Top>
-      <S.Bottom>
-        <S.ContentBox>
-          <S.Title>
-            <img src={Icon1} alt='아이콘'/>
-            <h2>네가 가진 고민은 이런 거야</h2>
-          </S.Title>
-          <div>
-            <p>{chatResult.first}</p>
-          </div>
-        </S.ContentBox>
-        <S.ContentBox>
-          <S.Title>
-          <img src={Icon2} alt='아이콘'/>
-            <h2>내가 생각하는 제안은 이런 것이야</h2>
-          </S.Title>
-          <div>
-            <p>{chatResult.second}</p>
-          </div>
-        </S.ContentBox>
-        <S.ContentBox>
-          <S.Title>
-          <img src={Icon3} alt='아이콘'/>
-            <h2>이것을 하면 기분이 좋아질 거야</h2>
-          </S.Title>
-          <div>
-            <p>{chatResult.third[0]}</p>
-          </div>
-          <div>
-            <p>{chatResult.third[1]}</p>
-          </div>
-          <div>
-            <p>{chatResult.third[2]}</p>
-          </div>
-        </S.ContentBox>
-      </S.Bottom>
-    </S.Container>
+        <S.Bottom>
+          <S.ContentBox>
+            <S.Title>
+              <img src={Icon1} alt='아이콘'/>
+              <h2>네가 가진 고민은 이런 거야</h2>
+            </S.Title>
+            <div>
+              <p>{chatResult.first}</p>
+            </div>
+          </S.ContentBox>
+          <S.ContentBox>
+            <S.Title>
+            <img src={Icon2} alt='아이콘'/>
+              <h2>내가 생각하는 제안은 이런 것이야</h2>
+            </S.Title>
+            <div>
+              <p>{chatResult.second}</p>
+            </div>
+          </S.ContentBox>
+          <S.ContentBox>
+            <S.Title>
+            <img src={Icon3} alt='아이콘'/>
+              <h2>이것을 하면 기분이 좋아질 거야</h2>
+            </S.Title>
+            <div>
+              <p>{chatResult.third[0]}</p>
+            </div>
+            <div>
+              <p>{chatResult.third[1]}</p>
+            </div>
+            <div>
+              <p>{chatResult.third[2]}</p>
+            </div>
+          </S.ContentBox>
+        </S.Bottom>
+      </S.Container>
+      <S.BtnBox>
+        <button onClick={handleDownload}></button>
+        <button>종료하기</button>
+      </S.BtnBox>
     </S.App>
   )
 }
--- src/components/Chatbot/ChatbotResult/ChatbotResult.style.js
+++ src/components/Chatbot/ChatbotResult/ChatbotResult.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import theme from '../../../constants/theme'
 import background from '../../../assets/chatbot/result/background.png'
 import paper from '../../../assets/chatbot/result/paper.png'
+import download from '../../../assets/chatbot/result/download.png'
 
 const {ALIGN, RESPONSIVE_SIZE} = theme;
 
@@ -18,34 +19,33 @@ export const App = styled.div`
 `
 export const Container = styled.div`
   background-image: url(${paper});
-  background-size: cover;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
+
   width: 80.5625rem;
   height: 49.80088rem;
   
   @media (max-width: 1440px) {
     width: 60.5625rem;
-    height: 38.0625rem;
+    height: 34.5rem;
   }
   @media (max-width: ${RESPONSIVE_SIZE.MOBILE}) {
     width: 30rem;
     height: 50rem;
   }
   border-radius: 20px; // 직사각형 부분의 모서리를 둥글게 설정
-  position: relative;
-  padding: 1.5%;
+  padding: 4%;
 `;
 
 export const Top = styled.div`
     display: flex;
-
+    // height: 35%;
 `
 
 export const Header = styled.div`
   border-radius: 50%; // 원형을 만들기 위해 50%로 설정
-  // position: absolute; // 원을 컨테이너의 상대적 위치에 놓기 위해 사용
-  // top: calc(0% + 25px); // 컨테이너 상단에서 50px 위로 이동
-  // left: calc(0% + 32px); // 컨테이너 왼쪽에서 20px 떨어뜨림
+  width: 20%;
   @media (max-width: ${RESPONSIVE_SIZE.MOBILE}) {
       width: 30rem;
       height: 50rem;
@@ -60,25 +60,65 @@ export const Header = styled.div`
       width: 30rem;
       height: 50rem;
     }
-    flex-shrink: 0;
   }
 `;
 
 export const Name = styled.div` // 000의 일지
-    margin-left: 13rem; 
-    div{}
-    h3 {}
+    margin: 5rem 0 0 1rem; 
+    div {
+      ${ALIGN.COLUMN_CENTER};
+      width: 13.125rem;
+      height: 3.0625rem;
+      border-radius: 3.8125rem;
+      background: #ED6243;
+      
+      color: #FFF;
+      font-family: SeoulHangang;
+      font-size: 1.75rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: -0.0175rem;
+    }
+    h3 {
+      margin-top: 0.5rem;
+      margin-left: 2rem;
+      color: #000;
+      font-family: SeoulHangang;
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      letter-spacing: -0.01rem;
+    }
 `
 
 export const Bottom = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr); /* 3개의 동일한 열 */
   gap: 1rem;
-
+  width: 100%;
+  height: 60%;
+  padding: 0 2%;
+  margin-top: 1%;
 `;
 
 export const ContentBox = styled.div`
-  div {}
+  width: 100%;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 1.125rem;
+  @media (max-width: 1440px) {
+    font-size: 0.6rem;
+  }
+  @media (max-width: ${RESPONSIVE_SIZE.MOBILE}) {
+    font-size: 0.75rem;
+  }
+  font-style: normal;
+  font-weight: 300;
+  line-height: 1.625rem; /* 144.444% */
+  div {
+  }
 `
 
 export const Title = styled.div`
@@ -86,7 +126,6 @@ export const Title = styled.div`
   width: 100%;
   img {
     width: 10%;
-  
     }
   h2 {
     width: 90%;
@@ -94,6 +133,12 @@ export const Title = styled.div`
     color: #ED6243;
     font-family: Pretendard;
     font-size: 1.25rem;
+    @media (max-width: 1440px) {
+      font-size: 1rem;
+    }
+    @media (max-width: ${RESPONSIVE_SIZE.MOBILE}) {
+      font-size: 0.75rem;
+    }
     font-style: normal;
     font-weight: 300;
     line-height: normal;
@@ -101,8 +146,39 @@ export const Title = styled.div`
 `
 
 export const BtnBox = styled.div`
-    button {
-    &:first-child {}
-    $:last-child {}
+  ${ALIGN.ROW_CENTER};
+  width: 100%;
+  gap: 2%;
+  margin-top: 1%;
+  button {
+    &:first-child {
+      background-image: url(${download});
+      background-size: cover;
+      background-position: center;
+      width: 3.75rem;
+      height: 3.75rem;
+      
+      box-shadow: 0px 4px 6px #E3DECB;
+      border: none;
+      border-radius: 50%;
     }
+    &:last-child {
+      ${ALIGN.COLUMN_CENTER};
+      width: 23.125rem;
+      padding: 1.3125rem 0rem 1.0625rem 0rem;
+
+      border-radius: 0.9375rem;
+      border: none;
+      background: #ED6243;
+
+      color: #FFF;
+      text-align: center;
+      font-family: Pretendard;
+      font-size: 1.125rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      letter-spacing: -0.03375rem;
+    }
+  }
 `

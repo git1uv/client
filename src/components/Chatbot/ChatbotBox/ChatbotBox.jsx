517e2981e52bca28f4a675a3ddca76c1cc79257d
youngju6143 <youngju6143@naver.com>
2024-09-16 02:24:20 +0900

    fix: 사용자/챗봇 메시지 state 형식 변경

--- src/components/Chatbot/ChatbotBox/ChatbotBox.jsx
+++ src/components/Chatbot/ChatbotBox/ChatbotBox.jsx
@@ -5,70 +5,78 @@ import Chat from '../Chat/Chat'
 import axios from 'axios';
 
 export default function ChatbotBox() {
-  const [isChat, setIsChat] = useState(false);
-  const [sentence, setSentence] = useState();
-  const [sendSentence, setSendSentence] = useState();
+  const [isChat, setIsChat] = useState(false); // 채팅 시작 여부
+  const [message, setMessage] = useState([]); // 사용자와 챗봇이 보낸 메시지들
+  const [input, setInput] = useState(); // input 값
+  const [current, setCurrent] = useState(); // 보낼 문장
   const [counseling, setCounseling] = useState({
     counselingId: 1,
     emotion: '평온',
-    message: [],
-  })
+  }) // 챗봇의 상태와 상담일지 ID
 
   const inputRef = useRef(null);
 
   const receivedToken = localStorage.getItem('token');
 
-  const textClear = () => {
+  const textClear = () => { // input 창 초기화
     inputRef.current.value = null;
   }
-  const ChangeChat = () => {
+
+  const ChangeChat = () => { // 채팅 화면으로 변경
     if (!isChat) {
       setIsChat(true);
     }
     textClear();
     // postChatting(); // 사용자 메시지 보내기 API
   }
-  const handleKeyPress = (e) => {
+
+  const handleKeyPress = (e) => { // 엔터키 누르면 메시지 전송
     if (e.key === 'Enter') {
       ChangeChat();
-      setSendSentence(sentence);
+      setMessage((prev) => [...prev, {
+        msg: input,
+        isUser: true,
+      }]);
+      setCurrent(input);
       textClear();
       // postChatting();  // 사용자 메시지 보내기 API
+      console.log(message)
     }
   }
 
   const ChangeInput = (e) => {
-    setSentence(e.target.value);
+    setInput(e.target.value);
   }
 
   /* 사용자 메시지 보내기 API*/
-  // const postChatting = async() => {
-  //   try {
-  //     const res = await axios.post(`/api/v1/chatbot/chatting`, {
-  //       userMessage: sendSentence,
-  //     }, {
-  //       headers: {
-  //         'Authorization': `Bearer ${receivedToken}`
-  //       }
-  //     })
-  //     console.log(res.data);
-  //     setCounseling((prev) => ({
-  //       ...prev, // 기존 객체의 나머지 필드들 유지
-  //       counselingId: res.data.counselingId,
-  //       emotion: res.data.emotion,
-  //       message: [...prev.message, ...res.data.message], // 기존 message 배열에 새로운 message 배열을 추가
-  //     }));
-      
-  //   } catch(err) {
-  //     console.log(err);
-  //     window.alert('메시지 전송을 실패하였습니다. 다시 시도해주세요.');
-  //   }
-  // }
+  const postChatting = async() => {
+    try {
+      const res = await axios.post(`/api/v1/chatbot/chatting`, {
+        userMessage: current,
+      }, {
+        headers: {
+          'Authorization': `Bearer ${receivedToken}`
+        }
+      })
+      console.log(res.data);
+      setCounseling({
+        counselingId: res.data.counselingId,
+        emotion: res.data.emotion,
+      });
+      setMessage((prev) => [...prev, {
+        msg: res.data.message,
+        isUser: false
+      }]);
+    } catch(err) {
+      console.log(err);
+      window.alert('메시지 전송을 실패하였습니다. 다시 시도해주세요.');
+    }
+  }
 
   return (
     <S.Container>
       {isChat ? (
-        <Chat sentence={sendSentence} counseling={counseling}/>
+        <Chat message={message} counseling={counseling}/>
       ) : (
         <>
           <S.Title>

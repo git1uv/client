395a6c13f6273f72f0c520c404b5dfbb4d357c1a
hyeran1216 <hran1216@khu.ac.kr>
2024-09-18 19:00:12 +0900

    test: 예시 데이터 추가

---
+++ src/datas/mail.js
@@ -0,0 +1,61 @@
+const MailData = [
+    {
+      mailId: 1,
+      content: "힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내힘내",
+      chatbotType: "F",
+      createdAt: "2024-09-17",
+      read: true,
+      starred: false
+    },
+    {
+      mailId: 2,
+      content: "안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽안뇽",
+      chatbotType: "T",
+      createdAt: "2024-09-18",
+      read: false,
+      starred: true
+    },
+    {
+      mailId: 4,
+      content: "안녕하세요, 야식과 수면에 대해 고민이 많으셨죠. 이제 그 고민들이 해결되었길 바랍니다.",
+      chatbotType: "H",
+      createdAt: "2024-09-20",
+      read: false,
+      starred: false
+    },
+    {
+      mailId: 5,
+      content: "안녕하세요. 배고픔과 졸음으로 고민하셨죠? 이 고민들이 해결되었길 바랍니다.",
+      chatbotType: "H",
+      createdAt: "2024-09-20",
+      read: false,
+      starred: false
+    },
+    {
+      mailId: 6,
+      content: "안녕하세요",
+      chatbotType: "H",
+      createdAt: "2024-09-20",
+      read: false,
+      starred: false
+    },
+    {
+      mailId: 7,
+      content: "ㅎㅎ",
+      chatbotType: "H",
+      createdAt: "2024-09-20",
+      read: false,
+      starred: false
+    },
+    {
+      mailId: 8,
+      content: "ㅎㅎ",
+      chatbotType: "H",
+      createdAt: "2024-09-20",
+      read: false,
+      starred: false
+    }
+  ];
+  
+  export default MailData;
+  

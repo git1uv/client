9b89c38a9160b6f35aff940bdba771aa9bbbd623
hyeran1216 <hran1216@khu.ac.kr>
2024-09-20 21:12:28 +0900

    feat: 편지 목록 조회 api

--- src/routes/Letter.jsx
+++ src/routes/Letter.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import * as L from "../components/Letter/Letter.style";
-import { FaHeart, FaTrash} from "react-icons/fa";
 import Letter from '../components/Modal/Letter/ChatLetter';
 import styled from "styled-components";
 import axios from 'axios';
 import {Fface, Tface, Hface, heart, Emptyheart} from '../assets/letterImg/icons';
 import DeleteLetterModal from '../components/Modal/Letter/DeleteLetter';
-import data from '../datas/mail';
+import dummyMails from '../datas/mail'; 
 
 const LetterWrapper = styled.div`
   display: flex;
@@ -18,6 +17,7 @@ const LetterWrapper = styled.div`
 `;
 
 function Mailbox() {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const [isLetterModalVisible, setLetterModalVisible] = useState(false);
   const [mails, setMails] = useState([]);
   const [seeAllActive, setSeeAllActive] = useState(true);
@@ -28,18 +28,41 @@ function Mailbox() {
   const [isDeleteModalVisible, setDeleteModalVisible] = useState(false);
   const [selectedMailIds, setSelectedMailIds] = useState([]);  
 
-  const handleLetterClick = async (mailId) => {
-    //임시
-    const mail = data.find(mail => mail.mailId === mailId);
-    if (mail) {
-      setMailDetails(mail); 
-      setLetterModalVisible(true); 
-    }
-    /**try {
-      const response = await axios.get(`/api/v1/mail?mailId=${mailId}`);
+  const accessToken = localStorage.getItem('accessToken'); 
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  const fetchMails = async (listType = '') => {
+    try {
+      const response = await axios.get(`${serverURL}/api/v1/mail/list${listType ? `?listType=${listType}` : ''}`,
+        {
+          headers: {
+            'Authorization': `Bearer ${accessToken} ${refreshToken}`
+          },
+        }
+      );
       if (response.data.code === "200") {
-        setMailDetails(response.data.data);  // 메일 상세 정보를 상태에 저장
-        setLetterModalVisible(true);  // 모달 열기
+        setMails(response.data.data.mails);
+      } else if (response.data.code === "MAIL5001") {
+        console.error("편지 가져오기 실패:", response.data.message);
+      } else {
+        console.error("편지가 없습니다:", response.data.message);
+      }
+    } catch (error) {
+      console.error("API 호출 중 오류 발생:", error);
+    }
+  };
+
+  const handleLetterClick = async (mailId) => {
+    try {
+      const response = await axios.get(`${serverURL}/api/v1/mail?mailId=${mailId}`,
+        {
+          headers: {
+            'Authorization': `Bearer ${accessToken} ${refreshToken}`
+          },
+        });
+      if (response.data.code === "200") {
+        setMailDetails(response.data.data); 
+        setLetterModalVisible(true); 
       } else if(response.data.code === "MAIL5001"){
         console.error("편지 가져오기 실패:", response.data.message);
       } else {
@@ -47,7 +70,7 @@ function Mailbox() {
       }
     } catch (error) {
       console.error("API 호출 중 오류 발생:", error);
-    }*/
+    }
   };
 
   const handleDeleteClick = () => {
@@ -59,45 +82,17 @@ function Mailbox() {
     }
   };
 
-  // 예시 데이터 설정
-  const fetchMailsForType = (listType = "all") => {
-    switch (listType) {
-      case "starred":
-        return data.filter(mail => mail.starred);
-      case "notRead":
-        return data.filter(mail => !mail.read);
-      case "all":
-      default:
-        return data;
-    }
-  };
-
-/**useEffect(() => {
-    const fetchMails = async (listType = '') => {
-      try {
-        const response = await axios.get(`/api/v1/mail/list${listType ? `?listType=${listType}` : ''}`);
-        if (response.data.code === "200") {
-          setMails(response.data.data.mails); 
-        } else if (response.data.code === "MAIL5001") {
-          console.error("편지 가져오기 실패:", response.data.message);
-        } else {
-          console.error("편지가 없습니다", response.data.message);
-        }
-      } catch (error) {
-        console.error("API 호출 중 오류 발생:", error);
-      }
-    };
-    fetchMails('');
-  }, []);
-
- */
   const toggleFavorite = (mailId) => {
     const updatedMails = mails.map((mail) =>
       mail.mailId === mailId ? { ...mail, starred: !mail.starred } : mail
     );
     setMails(updatedMails);
-    /**
-    axios.patch(`/api/v1/mail/star/${mailId}`)
+    axios.patch(`${serverURL}/api/v1/mail/star/${mailId}`,{},
+      {
+        headers: {
+          'Authorization': `Bearer ${accessToken} ${refreshToken}`
+        },
+      })
       .then(response => {
         if (response.data.code === "200") {
           console.log("즐겨찾기 추가 성공");
@@ -108,8 +103,7 @@ function Mailbox() {
         }
       })
       .catch(error => console.error("즐겨찾기 추가 실패", error));
-    */
-};
+  };
 
 
   const getChatbotImage = (chatbotType) => {
@@ -129,26 +123,19 @@ function Mailbox() {
     setSeeAllActive(true);
     setSeeFavoritesActive(false);
     setSeeNotReadActive(false);
-    //fetchMails('all'); 
-
-    setMails(fetchMailsForType("all"));
+    fetchMails('all'); 
     };
   const handleSeeFavoritesToggle = () => {
-    //fetchFavoriteMails();
     setSeeFavoritesActive(true);
     setSeeAllActive(false);
     setSeeNotReadActive(false);
-    //fetchMails('starred');  
-
-    setMails(fetchMailsForType("starred"));
+    fetchMails('starred');  
     };
   const handleSeeNotReadToggle = () => {
     setSeeNotReadActive(true);
     setSeeAllActive(false);
     setSeeFavoritesActive(false);
-    //fetchMails('notRead');  
-
-    setMails(fetchMailsForType("notRead"));
+    fetchMails('notRead'); 
     };
 
   const handleCheck = (mailId) => {
@@ -160,13 +147,11 @@ function Mailbox() {
       }
     });
   };
-    
-  useEffect(() => {
-    //예시
-    setMails(fetchMailsForType("all"));
-  }, []); 
-  
 
+  useEffect(() => {
+    fetchMails();
+  }, []);
+  
   return (
     <LetterWrapper>
     <L.Container>

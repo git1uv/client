c82e8dc56165d78748ac7bfae10b5a8af85bd8ef
youngju6143 <youngju6143@naver.com>
2024-09-22 04:29:21 +0900

    design: 채팅화면 헤더 디자인

--- src/components/Chatbot/Mobile/ChatbotMobile.style.js
+++ src/components/Chatbot/Mobile/ChatbotMobile.style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import theme from '../../../constants/theme';
+import { IoIosArrowBack } from "react-icons/io";
 
 const { ALIGN, RESPONSIVE_SIZE } = theme;
 
@@ -8,9 +9,55 @@ export const App = styled.div`
     height: 100dvh;
     background: #F5F4F2;
 `
+
+/* Header */
 export const Header = styled.div`
-    width: 100vw;
-    height: 2.5rem;
+    display: grid;
+    // justify-content: space-between;
+    align-items: center;
+    grid-template-columns: repeat(3, 1fr); /* 3개의 동일한 열 */
+    gap: 6rem;
+
+    padding: 0 1rem;
+
+    width: 100%;
+    height: 4rem;
     background: #F5F4F2;
-    box-shadow: 0px 2px 2px #A5A5A5;
+    box-shadow: 0px 2px 2px #e0e0e0;
+
+    h1 {
+        margin: 0;
+        color: #27272A;
+        font-family: SeoulHangang;
+        font-size: 2rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: -0.0125rem;
+    }
+`   
+
+export const BackBtn = styled.button`
+    ${ALIGN.ROW_CENTER};
+    margin-left: -2rem;
+    width: 5rem;
+    color: #ED6243;
+    border: none;
+    background: transparent;
+`
+
+export const EndBtn = styled.div`
+    ${ALIGN.ROW_CENTER};
+    width: 6rem;
+    height: 2.8rem;
+
+    border-radius: 4.8125rem;
+    background: #ED6243;
+    border: none;
+
+    color: white;
+    font-family: Pretendard;
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: normal;
 `
--- src/components/Chatbot/Mobile/Header/Header.jsx
+++ src/components/Chatbot/Mobile/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import * as S from '../ChatbotMobile.style'
+import { IoIosArrowBack } from "react-icons/io";
 
 export default function Header() {
   return (
-    <S.Header />
+    <S.Header>
+      <S.BackBtn>
+        <IoIosArrowBack size="30"/>
+      </S.BackBtn>
+      <h1>반바니</h1>
+      <S.EndBtn>끝내기</S.EndBtn>
+    </S.Header>
   )
 }

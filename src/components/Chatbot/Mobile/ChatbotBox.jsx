4dbdf7b6378140976b91642c19971f224e695fa2
youngju6143 <youngju6143@naver.com>
2024-09-23 04:28:35 +0900

    feat: 감정에 따른 표정 띄우기 구현

--- src/components/Chatbot/Mobile/ChatbotBox.jsx
+++ src/components/Chatbot/Mobile/ChatbotBox.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './ChatbotMobile.style'
-import chair from '../../../assets/chatbot/chatStart/ChairWeb.png'
+import chair from '../../../assets/chatbot/chatStart/ChairMobile.png'
 import SimmaeumImg from '../../../assets/chatbot/chatStart/Simmaeum.png'
 import BanbaniImg from '../../../assets/chatbot/chatStart/Banbani.png'
 import NeuraneeImg from '../../../assets/chatbot/chatStart/Neuranee.png'
 
+import { Simmaeum, Banbani, Neuranee } from '../../../datas/emotion'
+import { useSelector } from 'react-redux'
+
 export default function ChatbotBox() {
   const result = localStorage.getItem('result');
+  let counseling = useSelector((state) => state.counseling)
+
+  const [emotion, setEmotion] = useState(Neuranee.basic);
+
+  // 감정 분석에 따른 챗봇의 표정 변화 구현 
+  const changeFace = () => {
+    let select = Simmaeum;
+    if (result === 'Simmaeum')
+      select = Simmaeum;
+    else if (result === 'Banbani')
+      select = Banbani;
+    else
+      select = Neuranee;
+
+    switch(counseling.emotion) {
+      case '평온':
+        setEmotion(select.basic);
+        break;
+      case '웃음':
+        setEmotion(select.smile);
+        break;
+      case '사랑':
+        setEmotion(select.love);
+        break;
+      case '놀람':
+        setEmotion(select.surprise);
+        break;
+      case '슬픔':
+        setEmotion(select.sad);
+        break;
+      case '불편':
+        setEmotion(select.awkward);
+        break;
+      case '화남':
+        setEmotion(select.annoying);
+        break;
+      case '불안':
+        setEmotion(select.anxiety);
+        break;
+      case '피곤':
+        setEmotion(select.tired);
+        break;
+      default:
+        setEmotion(select.love);
+    }
+  }
+
 
   return (
     <S.ChatbotBox>
@@ -16,6 +66,7 @@ export default function ChatbotBox() {
         : result === 'Banbani' ? <S.Character src={BanbaniImg} alt='chatbot' /> 
         : <S.Character src={NeuraneeImg} alt='chatbot' /> 
       }
+      <S.Emotion src={emotion} />
     </S.ChatbotBox>
   )
 }

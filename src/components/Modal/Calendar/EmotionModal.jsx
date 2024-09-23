0a2dc36330c549e0e19aeb76843d775eef64841b
hyeran1216 <hran1216@khu.ac.kr>
2024-09-23 22:40:02 +0900

    feat: 오늘의 감정 저장 api

--- src/components/Modal/Calendar/EmotionModal.jsx
+++ src/components/Modal/Calendar/EmotionModal.jsx
@@ -4,26 +4,27 @@ import x from '../../../assets/x.png';
 import axios from 'axios';
 import {img1, img2, img3, img4, img5, img6, img7, img8, img9,img10, img11, img12, img13, img14, img15, img16} from '../../../assets/CalendarImg/icons';
 
-const EmotionModal = ({ isVisible, onClose, calendarID, setEmotion}) => {
+const EmotionModal = ({ isVisible, onClose, calendarId, setEmotion}) => {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
   const outside = useRef();
   const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16];
-  const emotionNames = ['Laughing', 'Excited', 'Passionate', 'Peaceful', 'Angry', 'Crying', 'Dissatisfied', 'Disappointed', 'Inlove', 'Sick', 'Proud', 'Tired', 'Surprised', 'Anxious', 'Happy', 'Embarrassed'];
+  const emotionNames = ['laughing', 'excited', 'passionate', 'peaceful', 'angry', 'crying', 'dissatisfied', 'disappointed', 'inlove', 'sick', 'proud', 'tired', 'surprised', 'anxious', 'happy', 'embarrassed'];
+
+  const accessToken = localStorage.getItem('accessToken'); 
+  const refreshToken = localStorage.getItem('refreshToken');
 
   if (!isVisible) return null;
 
-  // 예시 데이터
-  const handleEmotionSelect = (emotionName) => {
-    setEmotion(emotionName); 
-    onClose(); 
-  };
-
-  /**
   const handleEmotionSelect = async (emotionName) => {
     try {
-      const response = await axios.patch(`/api/v1/calendar/today/${calendarID}/emotion`, {
+      const response = await axios.patch(`${serverURL}/api/v1/calendar/today/${calendarId}/emotion`, {
         emotion: emotionName,
+      },{
+        headers: {
+          'Authorization': `Bearer ${accessToken} ${refreshToken}`
+        },
       });
-      if (response.status === 200) {
+      if (response.data.code === '200') {
         setEmotion(emotionName); 
         onClose();
       } else {
@@ -32,7 +33,7 @@ const EmotionModal = ({ isVisible, onClose, calendarID, setEmotion}) => {
     } catch (error) {
       console.error('감정 저장 중 오류 발생', error);
     }
-  };*/
+  };
 
   return (
     <M.ModalBg ref={outside} onClick={(e) => { if (e.target === outside.current) onClose(); }}>

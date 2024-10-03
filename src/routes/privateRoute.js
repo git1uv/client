f95dda375688c1081792cd97162f9f07dcf2ef20
youngju6143 <youngju6143@naver.com>
2024-10-03 21:45:33 +0900

    fix: 닉네임 변경 페이지 배경 안 뜨는 오류 수정

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -5,7 +5,7 @@ export default function PrivateRoute() {
   const isLogined = localStorage.getItem('accessToken') && localStorage.getItem('refreshToken');
 
   if (!isLogined) {
-    window.alert('이 페이지는 로그인한 사용자만 이용하실 수 있어요. 로그인 후 다시 시도해 주세요 😊');
+    window.alert('이 페이지는 로그인해야만 이용하실 수 있어요. 로그인 후 다시 시도해 주세요 😊');
   }
 
   return (

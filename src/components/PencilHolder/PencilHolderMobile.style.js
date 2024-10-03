2670afbd246ba73d78e939dfc46256a4eaf5b042
youngju6143 <youngju6143@naver.com>
2024-10-03 17:58:22 +0900

    design: pencilholder mobile 전체 수정

--- src/components/PencilHolder/PencilHolderMobile.style.js
+++ src/components/PencilHolder/PencilHolderMobile.style.js
@@ -1,13 +1,38 @@
 import styled from 'styled-components';
 import letter from "../../assets/pencilHolder/letter_mobile.webp"
 
+export const Title = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    align-self: flex-start;
+    margin-left: 2%;
+    h1 {
+        margin: 0 0 1% 0;
+        font-family: 'Pretendard';
+        font-weight: 500;
+        font-size: 1.25rem;
+        letter-spacing: -0.408px;
+        color: #000000;
+    }
+    p {
+        margin: 0;
+        font-family: 'Pretendard';
+        font-weight: 400;
+        font-size: 0.75rem;
+        letter-spacing: -0.408px;
+        color: #686868;
+    }
+`
+
 export const Letter = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-    width: 30rem;
-    height: 60vh;
+    width: 22.0625rem;
+
+    height: auto;
     margin-top: 2rem;
 
     background-image: url(${letter});
@@ -20,7 +45,7 @@ export const WriteBox = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    width: 24rem;
+    width: 90%;
     margin: 0.5rem 0 1rem 0;
 
     h3 {
@@ -34,7 +59,7 @@ export const WriteBox = styled.div`
 
     input {
         position: relative;
-        width: 22rem;
+        width: 100%;
         height: 3rem;
         border: none;
         border-radius: 0.3125rem;
@@ -42,10 +67,10 @@ export const WriteBox = styled.div`
         box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
 
         font-family: SeoulHangang;
-        font-size: 1.25rem;
+        font-size: 1rem;
         font-weight: 400;
         line-height: normal;
-        padding-left: 10px;
+        padding: 5%;
     }
 
     input::placeholder {
@@ -57,11 +82,8 @@ export const WriteBox = styled.div`
     }
 
     textarea {
-        width: 22rem;
-        height: 30vh;
-        @media (max-width: 1470px) {
-            height: 25vh;
-        }
+        width: 100%;
+        height: 25vh;
 
         border: none;
         border-radius: 1rem;
@@ -69,10 +91,10 @@ export const WriteBox = styled.div`
         box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
 
         font-family: SeoulHangang;
-        font-size: 1.25rem;
+        font-size: 1rem;
         font-weight: 400;
         line-height: normal;
-        padding: 10px;
+        padding: 5%;
 
         resize: none;
 

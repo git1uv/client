640da4ef52ba2e64cce63588aec456c06926ef22
youngju6143 <youngju6143@naver.com>
2024-11-26 23:26:10 +0900

    fix: emotion.js 업로드

--- src/datas/emotion.js
+++ src/datas/emotion.js
@@ -1,67 +1,67 @@
-import red_1_basic from '../assets/chatbot/emotion/Simmaeum/red_1_basic.webp'
-import red_2_smile from '../assets/chatbot/emotion/Simmaeum/red_2_smile.webp'
-import red_3_love from '../assets/chatbot/emotion/Simmaeum/red_3_love.webp'
-import red_4_surprise from '../assets/chatbot/emotion/Simmaeum/red_4_surprise.webp'
-import red_5_sad from '../assets/chatbot/emotion/Simmaeum/red_5_sad.webp'
-import red_6_awkward from '../assets/chatbot/emotion/Simmaeum/red_6_awkard.webp'
-import red_7_annoying from '../assets/chatbot/emotion/Simmaeum/red_7_annoying.webp'
-import red_8_anxiety from '../assets/chatbot/emotion/Simmaeum/red_8_anxiety.webp'
-import red_9_tired from '../assets/chatbot/emotion/Simmaeum/red_9_tired.webp'
+import red_1_basic from "../assets/chatbot/emotion/Simmaeum/red_1_basic.json";
+import red_2_smile from "../assets/chatbot/emotion/Simmaeum/red_2_smile.json";
+import red_3_love from "../assets/chatbot/emotion/Simmaeum/red_3_love.json";
+import red_4_surprise from "../assets/chatbot/emotion/Simmaeum/red_4_surprise.json";
+import red_5_sad from "../assets/chatbot/emotion/Simmaeum/red_5_sad.json";
+import red_6_awkward from "../assets/chatbot/emotion/Simmaeum/red_6_awkard.json";
+import red_7_annoying from "../assets/chatbot/emotion/Simmaeum/red_7_annoying.json";
+import red_8_anxiety from "../assets/chatbot/emotion/Simmaeum/red_8_anxiety.json";
+import red_9_tired from "../assets/chatbot/emotion/Simmaeum/red_9_tired.json";
 
-import yellow_1_basic from '../assets/chatbot/emotion/Banbani/yellow_1_basic.webp'
-import yellow_2_smile from '../assets/chatbot/emotion/Banbani/yellow_2_smile.webp'
-import yellow_3_love from '../assets/chatbot/emotion/Banbani/yellow_3_love.webp'
-import yellow_4_surprise from '../assets/chatbot/emotion/Banbani/yellow_4_surprise.webp'
-import yellow_5_sad from '../assets/chatbot/emotion/Banbani/yellow_5_sad.webp'
-import yellow_6_awkward from '../assets/chatbot/emotion/Banbani/yellow_6_awkard.webp'
-import yellow_7_annoying from '../assets/chatbot/emotion/Banbani/yellow_7_annoying.webp'
-import yellow_8_anxiety from '../assets/chatbot/emotion/Banbani/yellow_8_anxiety.webp'
-import yellow_9_tired from '../assets/chatbot/emotion/Banbani/yellow_9_tired.webp'
+import yellow_1_basic from "../assets/chatbot/emotion/Banbani/yellow_1_basic.json";
+import yellow_2_smile from "../assets/chatbot/emotion/Banbani/yellow_2_smile.json";
+import yellow_3_love from "../assets/chatbot/emotion/Banbani/yellow_3_love.json";
+import yellow_4_surprise from "../assets/chatbot/emotion/Banbani/yellow_4_surprise.json";
+import yellow_5_sad from "../assets/chatbot/emotion/Banbani/yellow_5_sad.json";
+import yellow_6_awkward from "../assets/chatbot/emotion/Banbani/yellow_6_awkward.json";
+import yellow_7_annoying from "../assets/chatbot/emotion/Banbani/yellow_7_annoying.json";
+import yellow_8_anxiety from "../assets/chatbot/emotion/Banbani/yellow_8_anxiety.json";
+import yellow_9_tired from "../assets/chatbot/emotion/Banbani/yellow_9_tired.json";
 
-import green_1_basic from '../assets/chatbot/emotion/Neuranee/green_1_basic.webp'
-import green_2_smile from '../assets/chatbot/emotion/Neuranee/green_2_smile.webp'
-import green_3_love from '../assets/chatbot/emotion/Neuranee/green_3_love.webp'
-import green_4_surprise from '../assets/chatbot/emotion/Neuranee/green_4_surprise.webp'
-import green_5_sad from '../assets/chatbot/emotion/Neuranee/green_5_sad.webp'
-import green_6_awkward from '../assets/chatbot/emotion/Neuranee/green_6_awkard.webp'
-import green_7_annoying from '../assets/chatbot/emotion/Neuranee/green_7_annoying.webp'
-import green_8_anxiety from '../assets/chatbot/emotion/Neuranee/green_8_anxiety.webp'
-import green_9_tired from '../assets/chatbot/emotion/Neuranee/green_9_tired.webp'
+import green_1_basic from "../assets/chatbot/emotion/Neuranee/green_1_basic.json";
+import green_2_smile from "../assets/chatbot/emotion/Neuranee/green_2_smile.json";
+import green_3_love from "../assets/chatbot/emotion/Neuranee/green_3_love.json";
+import green_4_surprise from "../assets/chatbot/emotion/Neuranee/green_4_surprise.json";
+import green_5_sad from "../assets/chatbot/emotion/Neuranee/green_5_sad.json";
+import green_6_awkward from "../assets/chatbot/emotion/Neuranee/green_6_awkard.json";
+import green_7_annoying from "../assets/chatbot/emotion/Neuranee/green_7_annoying.json";
+import green_8_anxiety from "../assets/chatbot/emotion/Neuranee/green_8_anxiety.json";
+import green_9_tired from "../assets/chatbot/emotion/Neuranee/green_9_tired.json";
 
 const Simmaeum = {
-    basic: red_1_basic,
-    smile: red_2_smile,
-    love: red_3_love, 
-    surprise: red_4_surprise,
-    sad: red_5_sad, 
-    awkward: red_6_awkward, 
-    annoying: red_7_annoying,
-    anxiety: red_8_anxiety,
-    tired: red_9_tired
+  basic: red_1_basic,
+  smile: red_2_smile,
+  love: red_3_love,
+  surprise: red_4_surprise,
+  sad: red_5_sad,
+  awkward: red_6_awkward,
+  annoying: red_7_annoying,
+  anxiety: red_8_anxiety,
+  tired: red_9_tired,
 };
-    
+
 const Banbani = {
-    basic: yellow_1_basic,
-    smile: yellow_2_smile,
-    love: yellow_3_love,
-    surprise: yellow_4_surprise,
-    sad: yellow_5_sad,
-    awkward: yellow_6_awkward,
-    annoying: yellow_7_annoying,
-    anxiety: yellow_8_anxiety,
-    tired: yellow_9_tired
+  basic: yellow_1_basic,
+  smile: yellow_2_smile,
+  love: yellow_3_love,
+  surprise: yellow_4_surprise,
+  sad: yellow_5_sad,
+  awkward: yellow_6_awkward,
+  annoying: yellow_7_annoying,
+  anxiety: yellow_8_anxiety,
+  tired: yellow_9_tired,
 };
 
 const Neuranee = {
-    basic: green_1_basic,
-    smile: green_2_smile,
-    love: green_3_love,
-    surprise: green_4_surprise,
-    sad: green_5_sad,
-    awkward: green_6_awkward,
-    annoying: green_7_annoying,
-    anxiety: green_8_anxiety,
-    tired: green_9_tired,
+  basic: green_1_basic,
+  smile: green_2_smile,
+  love: green_3_love,
+  surprise: green_4_surprise,
+  sad: green_5_sad,
+  awkward: green_6_awkward,
+  annoying: green_7_annoying,
+  anxiety: green_8_anxiety,
+  tired: green_9_tired,
 };
 
-export {Simmaeum, Banbani, Neuranee};
+export { Simmaeum, Banbani, Neuranee };
